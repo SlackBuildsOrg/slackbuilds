c2a937051883824c29fcd5345d0845628a8219d7
A. Tomasini <alto dot tom at gmail dot com>
2023-11-30 22:40:54 +0700

    academic/xschem: Update script.

--- academic/xschem/doinst.sh
+++ academic/xschem/doinst.sh
@@ -1,4 +1,6 @@
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
+ln -s /usr/share/doc/xschem /usr/doc/xschem-3.4.4
 

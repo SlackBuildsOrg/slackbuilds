f86889a1fc1db678ee6735ca56ad99bb5209410e
thnkman <thnkman@proton.me>
2024-01-02 07:28:20 +0100

    network/dnscrypt-proxy | Final touchup :)

--- network/dnscrypt-proxy/doinst.sh
+++ network/dnscrypt-proxy/doinst.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-# dnscrypt-proxy writes files to /etc/$PRGNAM after dropping
-# privileges. This ensures $DNSCRYPT_USER can write files
-# to its config /etc/$PRGNAM, without having to change default
-# root:root in slackware /etc/$PRGNAM
+# dnscrypt-proxy writes files to its data directories after 
+# dropping privileges. This ensures $DNSCRYPT_USER can write 
+# files to these directories, without having to change default
+# root:root in slackware.
 #
 # This is a workaround, might not be a elegant solution. 
 
+PRGNAM=dnscrypt-proxy
 DNSCRYPT_USER=$(cat /usr/doc/dnscrypt-proxy-2.1.5/dnscrypt-proxy.SlackBuild | grep -oP '\${DNSCRYPT_USER:-\K[^}]*')
-setfacl -m u:$DNSCRYPT_USER:rwx /etc/dnscrypt-proxy
+DNSCRYPT_GROUP=$(cat /usr/doc/dnscrypt-proxy-2.1.5/dnscrypt-proxy.SlackBuild | grep -oP '\${DNSCRYPT_GROUP:-\K[^}]*')
+
+chown $DNSCRYPT_USER:$DNSCRYPT_GROUP /etc/$PRGNAM/*
+
+setfacl -m u:$DNSCRYPT_USER:rwx /etc/$PRGNAM
+setfacl -m u:$DNSCRYPT_USER:rwx /var/run/$PRGNAM
+setfacl -m u:$DNSCRYPT_USER:rwx /var/log/$PRGNAM

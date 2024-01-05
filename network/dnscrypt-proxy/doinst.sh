4ed632b5aebff7efdbd6e54b39d7607207d5eb6f
thnkman <thnkman@proton.me>
2024-01-05 15:23:37 +0100

    network/dnscrypt-proxy

--- network/dnscrypt-proxy/doinst.sh
+++ network/dnscrypt-proxy/doinst.sh
@@ -8,6 +8,7 @@
 # This is a workaround, might not be a elegant solution.
 
 PRGNAM=dnscrypt-proxy
+<<<<<<< HEAD
 DNSCRYPT_UID=dnscrypt
 DNSCRYPT_GID=dnscrypt
 
@@ -16,3 +17,13 @@ chown $DNSCRYPT_UID:$DNSCRYPT_GID etc/$PRGNAM/*
 setfacl -m u:$DNSCRYPT_UID:rwx etc/$PRGNAM
 setfacl -m u:$DNSCRYPT_UID:rwx var/run/$PRGNAM
 setfacl -m u:$DNSCRYPT_UID:rwx var/log/$PRGNAM
+=======
+DNSCRYPT_USER=dnscrypt
+DNSCRYPT_GROUP=dnscrypt
+
+chown $DNSCRYPT_USER:$DNSCRYPT_GROUP /etc/$PRGNAM/*
+
+setfacl -m u:$DNSCRYPT_USER:rwx /etc/$PRGNAM
+setfacl -m u:$DNSCRYPT_USER:rwx /var/run/$PRGNAM
+setfacl -m u:$DNSCRYPT_USER:rwx /var/log/$PRGNAM
+>>>>>>> 8cc6fa4773 (network/dnscrypt-proxy)

ce8712feaa1f701dc13d872217ba9411bcd10e66
thnkman <thnkman@proton.me>
2024-01-07 11:41:41 +0100

    network/dnscrypt-proxy: Updated for new version

--- network/dnscrypt-proxy/doinst.sh
+++ network/dnscrypt-proxy/doinst.sh
@@ -8,8 +8,6 @@
 # This is a workaround, might not be a elegant solution.
 
 PRGNAM=dnscrypt-proxy
-<<<<<<< HEAD
-<<<<<<< HEAD
 DNSCRYPT_UID=dnscrypt
 DNSCRYPT_GID=dnscrypt
 
@@ -18,23 +16,3 @@ chown $DNSCRYPT_UID:$DNSCRYPT_GID etc/$PRGNAM/*
 setfacl -m u:$DNSCRYPT_UID:rwx etc/$PRGNAM
 setfacl -m u:$DNSCRYPT_UID:rwx var/run/$PRGNAM
 setfacl -m u:$DNSCRYPT_UID:rwx var/log/$PRGNAM
-=======
-DNSCRYPT_USER=dnscrypt
-DNSCRYPT_GROUP=dnscrypt
-=======
-DNSCRYPT_UID=dnscrypt
-DNSCRYPT_GID=dnscrypt
->>>>>>> a80e57600d (network/dnscrypt-proxy)
-
-chown $DNSCRYPT_UID:$DNSCRYPT_GID etc/$PRGNAM/*
-
-<<<<<<< HEAD
-setfacl -m u:$DNSCRYPT_USER:rwx /etc/$PRGNAM
-setfacl -m u:$DNSCRYPT_USER:rwx /var/run/$PRGNAM
-setfacl -m u:$DNSCRYPT_USER:rwx /var/log/$PRGNAM
->>>>>>> 8cc6fa4773 (network/dnscrypt-proxy)
-=======
-setfacl -m u:$DNSCRYPT_UID:rwx etc/$PRGNAM
-setfacl -m u:$DNSCRYPT_UID:rwx var/run/$PRGNAM
-setfacl -m u:$DNSCRYPT_UID:rwx var/log/$PRGNAM
->>>>>>> a80e57600d (network/dnscrypt-proxy)

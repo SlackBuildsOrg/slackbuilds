6f4e7562f32ca9d89f508248da87b32fedca24b8
Olivier Brouckaert <olivier.b@i-services.be>
2024-08-16 10:21:26 +0700

    office/eid-mw: Added (electronic proof of identity).

---
+++ office/eid-mw/doinst.sh
@@ -0,0 +1,13 @@
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi
+
+if [ -e /usr/lib/mozilla/pkcs11-modules ]; then
+  /usr/bin/ln -s /usr/lib/mozilla/pkcs11-modules /usr/lib64/mozilla/pkcs11-modules
+fi

754e015b2a6e430053fd7c3d165288b6f55d1f8d
Ruben Carlo Benante <rcb@beco.cc>
2025-07-11 07:29:54 +0700

    development/pacoteco: Added (Slackware Package Creator).

---
+++ development/pacoteco/doinst.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#***********************************************************************
+# (C)opyright 2025         by Ruben Carlo Benante                      *
+#   doinst.sh script for pacoteco version 20250628.222222              *
+#                                                                      *
+# This program is free software; you can redistribute it and/or modify *
+#   it under the terms of the GNU General Public License as published  *
+#   by the Free Software Foundation version 2 of the License.          *
+#                                                                      *
+# This program is distributed in the hope that it will be useful,      *
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of     *
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *
+#   GNU General Public License for more details.                       *
+#                                                                      *
+# You should have received a copy of the GNU General Public License    *
+#   along with this program; if not, write to the                      *
+#   Free Software Foundation, Inc.,                                    *
+#   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.          *
+#                                                                      *
+# Contact author at:                                                   *
+#   Ruben Carlo Benante                                                *
+#   rcb@beco.cc                                                        *
+#***********************************************************************
+
+# No .new config files to be handled by doinst.sh script in this package
+
+
+#-----------------------------------------------------------------------
+# vi: set ai et ts=4 sw=4 tw=0 wm=0 fo=croql syn=sh : Shell Vim modeline
+# Template by Dr. Beco <rcb at beco dot cc>      Version 20250704.084004

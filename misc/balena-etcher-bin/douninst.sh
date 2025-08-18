7e5598eab2312f03ec0246259ddef2a987349142
Anagnostakis Ioannis <elahistos@yahoo.gr>
2025-08-18 23:53:53 +0300

    Fix exec command , add dounist.sh
    Signed-off-by: Anagnostakis Ioannis <elahistos@yahoo.gr>

---
+++ misc/balena-etcher-bin/douninst.sh
@@ -0,0 +1,8 @@
+# douninst.sh
+#
+# uninstall script for Slackware >= 15.0
+#
+# NOTE: This script is run AFTER package removal, so be careful!
+#       Consider it optional, use if it is really needed.
+
+rm -rf /opt/balena-etcher

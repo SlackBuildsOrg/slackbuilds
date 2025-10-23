5ce080a48598e43dc73447341d41f79ff5a4296a
Antonio Henrique <ahlr_2000@yahoo.com>
2025-10-24 00:17:24 +0700

    development/cursor: Updated for version 1.7.28.

--- development/cursor/doinst.sh
+++ development/cursor/doinst.sh
@@ -1,4 +1,7 @@
-if [ -x /usr/bin/update-desktop-database ]; then
-  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+#!/bin/sh
+if command -v update-desktop-database 1>/dev/null 2>&1 ; then
+  update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi
-
+if command -v gtk-update-icon-cache 1>/dev/null 2>&1 ; then
+  gtk-update-icon-cache -q -t -f usr/share/icons/hicolor >/dev/null 2>&1
+fi

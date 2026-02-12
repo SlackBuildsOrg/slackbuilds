18d6d7c85bbf327de42bad90c0f62f38902e30ca
Artur Wrona <arturwrona91@gmail.com>
2026-02-12 21:56:10 +0700

    audio/qxgedit: Added (Qt GUI for MIDI).

---
+++ audio/qxgedit/doinst.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#===============================================================================
+# Nothing below here should be part of the build script itself.
+# It should be copied into a separate file 'doinst.sh' if you need it,
+# and then deleted from this SlackBuild script.
+#===============================================================================
+
+#===============================================================================
+# doinst.sh stuff - use/adapt as needed
+#===============================================================================
+
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+preserve_perms() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ -e $OLD ]; then
+    cp -a $OLD ${NEW}.incoming
+    cat $NEW > ${NEW}.incoming
+    mv ${NEW}.incoming $NEW
+  fi
+  config $NEW
+}
+
+schema_install() {
+  SCHEMA="$1"
+  GCONF_CONFIG_SOURCE="xml::etc/gconf/gconf.xml.defaults" \
+  chroot . gconftool-2 --makefile-install-rule \
+    /etc/gconf/schemas/$SCHEMA \
+    1>/dev/null
+}
+
+# WARNING
+# When upgradepkg is used, doinst.sh gets run twice. So don't put
+# anything here that takes a long time to run.

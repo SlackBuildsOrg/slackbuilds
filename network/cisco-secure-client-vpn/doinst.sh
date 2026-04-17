1f21634448986774f996c860f4ad823928ce9d0b
Reza Talebi <reza.talebi.73@outlook.com>
2026-04-17 22:28:50 +0700

    network/cisco-secure-client-vpn: Added (Cisco Secure Clien).

---
+++ network/cisco-secure-client-vpn/doinst.sh
@@ -0,0 +1,17 @@
+if [ -x etc/rc.d/rc.cisco-secure-client-vpn ]; then
+  chmod 0755 etc/rc.d/rc.cisco-secure-client-vpn
+fi
+
+if [ -x /usr/bin/update-desktop-database ]; then
+  /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
+fi
+
+if [ -x /usr/bin/update-mime-database ]; then
+  /usr/bin/update-mime-database usr/share/mime >/dev/null 2>&1
+fi
+
+if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
+  if [ -x /usr/bin/gtk-update-icon-cache ]; then
+    /usr/bin/gtk-update-icon-cache -f usr/share/icons/hicolor >/dev/null 2>&1
+  fi
+fi

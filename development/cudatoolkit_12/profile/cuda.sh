d50723518105254370d7843397587ef1c2be3241
Christoph Willing <chris.willing@linux.com>
2026-05-23 12:56:01 +1000

    development/cudatoolkit_12: Remove gcc14 dependence. Fix pkgconfig
    Signed-off-by: Christoph Willing <chris.willing@linux.com>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/cudatoolkit_12/profile/cuda.sh
+++ development/cudatoolkit_12/profile/cuda.sh
@@ -1,5 +1,5 @@
 export CUDA_PATH=/opt/cuda-@VERSION@
-export PATH="/opt/cuda-@VERSION@/bin:/opt/cuda-@VERSION@/nsight_compute:/opt/cuda-@VERSION@/nsight_systems/bin":"$PATH"
-export MANPATH="/opt/cuda-@VERSION@/gds/man":"$MANPATH"
-export PKG_CONFIG_PATH="/opt/cuda-@VERSION@/usr/lib64/pkgconfig/":"$PKG_CONFIG_PATH"
-export LD_LIBRARY_PATH="/opt/cuda-@VERSION@/lib64:/opt/cuda-@VERSION@/nvvm/lib64:/opt/cuda-@VERSION@/extras/CUPTI/lib64":"$LD_LIBRARY_PATH"
+export PATH="$CUDA_PATH/bin":"$CUDA_PATH/nsight_compute":"$CUDA_PATH/nsight_systems/bin":"$PATH"
+export MANPATH="$CUDA_PATH/gds/man":"$MANPATH"
+export PKG_CONFIG_PATH="$CUDA_PATH/usr/share/pkgconfig":"$PKG_CONFIG_PATH"
+export LD_LIBRARY_PATH="$CUDA_PATH/lib64":"$CUDA_PATH/nvvm/lib64":"$CUDA_PATH/extras/CUPTI/lib64":"$LD_LIBRARY_PATH"

b0c37c4edafcd338a691eac333d8e55d1dcaf9f9
thnkman <thnkman@proton.me>
2026-05-25 21:59:42 +0700

    network/electrs: Updated for version 0.11.1.

--- network/electrs/doinst.sh
+++ network/electrs/doinst.sh
@@ -10,3 +10,19 @@ setfacl -R -m u:$ELECTRS_UID:rwx etc/$PRGNAM
 setfacl -R -m u:$ELECTRS_UID:rwx var/lib/$PRGNAM
 setfacl -R -m u:$ELECTRS_UID:rwx var/log/$PRGNAM
 setfacl -R -m u:$ELECTRS_UID:rwx var/run/$PRGNAM
+
+# handle config.toml.new
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider…
+}
+config etc/$PRGNAM/config.toml.new
+

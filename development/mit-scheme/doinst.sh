c8059f3070416d92821c0cc6d2eca76d5f6849e5
B. Watson <urchlay@slackware.uk>
2026-06-05 01:22:48 -0400

    development/mit-scheme: Fix doinst.sh, writes to /usr.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/mit-scheme/doinst.sh
+++ development/mit-scheme/doinst.sh
@@ -1,21 +1,14 @@
-info_install() {
-  INFO="$1"
-  if [ -x /usr/bin/install-info ]; then
-    chroot . /usr/bin/install-info --info-dir=/usr/info $INFO 2> /dev/null
-  fi
-}
-
 if [ -x /usr/bin/install-info ]; then
   /usr/bin/install-info --info-dir=usr/info \
-                        /usr/info/mit-scheme-ffi.info.gz \
-                        /usr/info/mit-scheme-ref.info-1.gz \
-                        /usr/info/mit-scheme-ref.info-3.gz \
-                        /usr/info/mit-scheme-ref.info-5.gz \
-                        /usr/info/mit-scheme-sos.info.gz \
-                        /usr/info/mit-scheme-imail.info.gz \
-                        /usr/info/mit-scheme-ref.info-2.gz  \
-                        /usr/info/mit-scheme-ref.info-4.gz \
-                        /usr/info/mit-scheme-ref.info.gz  \
-                        /usr/info/mit-scheme-user.info.gz \
+                        usr/info/mit-scheme-ffi.info.gz \
+                        usr/info/mit-scheme-ref.info-1.gz \
+                        usr/info/mit-scheme-ref.info-3.gz \
+                        usr/info/mit-scheme-ref.info-5.gz \
+                        usr/info/mit-scheme-sos.info.gz \
+                        usr/info/mit-scheme-imail.info.gz \
+                        usr/info/mit-scheme-ref.info-2.gz  \
+                        usr/info/mit-scheme-ref.info-4.gz \
+                        usr/info/mit-scheme-ref.info.gz  \
+                        usr/info/mit-scheme-user.info.gz \
                         1> /dev/null 2>&1
 fi

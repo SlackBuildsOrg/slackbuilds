ea185ced138f523cb854e9279a182a433621a041
B. Watson <urchlay@slackware.uk>
2026-06-05 22:51:27 -0400

    system/isight-firmware-tools: Fix doinst.sh and man page.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- system/isight-firmware-tools/doinst.sh
+++ system/isight-firmware-tools/doinst.sh
@@ -1,5 +1,4 @@
-if [ -x usr/bin/install-info ]; then
-  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/ift-export.info.gz 2> /dev/null
-  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/ift-extract.info.gz 2> /dev/null
+if [ -x /usr/bin/install-info ]; then
+  /usr/bin/install-info --info-dir=usr/info usr/info/ift-export.info.gz 1> /dev/null 2>&1
+  /usr/bin/install-info --info-dir=usr/info usr/info/ift-extract.info.gz 1> /dev/null 2>&1
 fi
-

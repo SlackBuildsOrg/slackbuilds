166904947ec2db08d685df2b9638530602030f2c
B. Watson <urchlay@slackware.uk>
2026-06-05 21:43:19 -0400

    libraries/mpir: Fix doinst.sh.
    Signed-off-by: B. Watson <urchlay@slackware.uk>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- libraries/mpir/doinst.sh
+++ libraries/mpir/doinst.sh
@@ -1,5 +1,3 @@
 if [ -x /usr/bin/install-info ]; then
-  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/mpir.info.gz 2> /dev/null
-  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/mpir.info-1.gz 2> /dev/null
-  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/mpir.info-2.gz 2> /dev/null
+  /usr/bin/install-info --info-dir=usr/info usr/info/mpir.info.gz 1> /dev/null 2>&1
 fi

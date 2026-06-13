5b7ee151e2bbba2e8bac39895027ebda3f8cb095
Willy Sudiarto Raharjo <willysr@slackbuilds.org>
2026-06-13 07:38:55 +0700

    development/lm-studio-bin: Removed (Need newer glibc).
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- development/lm-studio-bin/lm-studio-bin.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#LD_LIBRARY_PATH=/opt/lm-studio-bin:$LD_LIBRARY_PATH
-env -i DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY XDG_CURRENT_DESKTOP=GNOME /opt/LM-Studio-*.AppImage
-#--no-sandbox

c12e60d839ad090f5c43f4da681e95f5174dc68e
Matteo Bernardini <ponce@slackbuilds.org>
2026-06-23 11:06:45 +0200

    network/squid: Updated for version 7.6.
    Fixes the SquidBleed vulnerability
    
    Signed-off-by: Matteo Bernardini <ponce@slackbuilds.org>
    
    Signed-off-by: Willy Sudiarto Raharjo <willysr@slackbuilds.org>

--- network/squid/doinst.sh
+++ network/squid/doinst.sh
@@ -25,7 +25,6 @@ preserve_perms() {
 preserve_perms etc/rc.d/rc.squid.new
 preserve_perms etc/squid/mime.conf.new
 preserve_perms etc/squid/squid.conf.new
-preserve_perms etc/squid/cachemgr.conf.new
 preserve_perms etc/squid/errorpage.css.new
 preserve_perms etc/logrotate.d/squid.new
 

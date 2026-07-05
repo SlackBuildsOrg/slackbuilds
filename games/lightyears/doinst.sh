bba4836657b9b3fa55e0306ef4b64004ec2737ce
Emmanuel N. Millán <emmanueln@gmail.com>
2026-07-05 12:23:21 -0300

    games/lightyears: Update version to 1.5.2 and new maintainer

--- games/lightyears/doinst.sh
+++ games/lightyears/doinst.sh
@@ -1,3 +1,5 @@
+ln -sf /usr/share/games/lightyears-1.5.2/lib20k /usr/share/games/lightyears-1.5.2/code 
+
 if [ -x /usr/bin/update-desktop-database ]; then
   /usr/bin/update-desktop-database -q usr/share/applications >/dev/null 2>&1
 fi

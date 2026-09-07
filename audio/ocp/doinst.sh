39a1fad09c48a58e084d4e51f2c6be87c251f2af
SPN Team <pwmn-master@hotmail.com>
2026-09-07 09:53:41 +0700

    audio/ocp: Updated for version 3.5.0.

--- audio/ocp/doinst.sh
+++ audio/ocp/doinst.sh
@@ -1,5 +1,5 @@
 if [ -x /usr/bin/install-info ]; then
-  /usr/bin/install-info --info-dir=usr/info usr/info/ocp.gz 1> /dev/null 2>&1
+  chroot . /usr/bin/install-info --info-dir=/usr/info /usr/info/ocp.info.gz 2> /dev/null
 fi
 
 if [ -e usr/share/icons/hicolor/icon-theme.cache ]; then
